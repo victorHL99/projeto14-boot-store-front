474268f3e50cb0ea1c9b62e9ec5dc263df06375e
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-10 16:35:08 -0300

    Css global habilitado e rodando

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 
+import './assets/css/reset.css';
+import './assets/css/global.css';
+
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
