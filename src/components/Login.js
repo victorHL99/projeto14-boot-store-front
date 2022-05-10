820bdc064cea52291ed4b9f7c548a25ec973479d
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-10 17:52:58 -0300

    Enviar informações do formulario para o backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,66 @@
+import { useState } from 'react';
+
+import styled from 'styled-components';
+
+import api from '.././api/api.js';
+
 function Login (){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmitLogin(e){
+        e.preventDefault();
+
+        try {
+            await api.post('/login', {
+                email,
+                password
+            });
+
+
+
+        } catch (error) {
+
+        }
+
+    }
 
     return (
+        <>
+            <header>teste</header> {/* FIXME VAI FAZER O HEADER AINDA */}
+            <Main>
+                <form>
+                    {/* EMAIL */}
+                    <input 
+                        type="text" 
+                        value={email}
+                        placeholder="Email"
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
 
-        <h1> eu sou o login</h1>
+                    {/* SENHA */}
+                    <input 
+                        type="text" 
+                        value={password}
+                        placeholder="Senha"
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+
+                    {/* BOTÃO ACESSAR CONTA */}
+                    <button type="submit" onClick={handleSubmitLogin} >Acessar conta</button>
+                </form>
+
+            </Main>
+        </>
+
     )
 }
 
 export default Login;
+
+const Main = styled.main`
+    width: 100vw;
+    height: 100vh;
+
+
+`
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './components/App'
 
 import './assets/css/reset.css';
 import './assets/css/global.css';
-
-
+import './components/api/api.js';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
