9e82f1a1073e8feaf7cca0676f8e8fa0c92f2af6
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-10 21:12:05 -0300

    Parte de login base criada

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 import styled from 'styled-components';
 
-import api from '.././api/api.js';
+import Header from './Header';
+/* import api from '.././api/api.js'; */
 
 function Login (){
     const [email, setEmail] = useState("");
@@ -27,29 +28,20 @@ function Login (){
 
     return (
         <>
-            <header>teste</header> {/* FIXME VAI FAZER O HEADER AINDA */}
+            <Header/> 
             <Main>
+                <TitleClient>Já sou cliente</TitleClient>
                 <form>
                     {/* EMAIL */}
-                    <input 
-                        type="text" 
-                        value={email}
-                        placeholder="Email"
-                        onChange={(e)=>setEmail(e.target.value)}
-                    />
+                    <Input type="text" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
 
                     {/* SENHA */}
-                    <input 
-                        type="text" 
-                        value={password}
-                        placeholder="Senha"
-                        onChange={(e)=>setPassword(e.target.value)}
-                    />
+                    <Input type="text" value={password} placeholder="Senha" onChange={(e)=>setPassword(e.target.value)}/>
 
                     {/* BOTÃO ACESSAR CONTA */}
-                    <button type="submit" /* onClick={handleSubmitLogin} */ >Acessar conta</button>
+                    <Button type="submit" /* onClick={handleSubmitLogin} */ ><p>Acessar conta</p></Button>
                 </form>
-
+                <TitleRegister>Criar conta</TitleRegister>
             </Main>
         </>
 
@@ -59,8 +51,89 @@ function Login (){
 export default Login;
 
 const Main = styled.main`
+    position:absolute ;
     width: 100vw;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+const TitleClient = styled.p`
+    position: relative;
+    top:50px;
+    left: 25px;
+    font-size: 20px;
+    font-family: 'Saira Stencil One', cursive;
+    color: #000000;
+    cursor: hidden;
 
+`
+const TitleRegister = styled.p`
+    position: relative;
+    top:170px;
+    left: 25px;
+    font-size: 20px;
+    font-family: 'Saira Stencil One', cursive;
+    color: #000000;
 
+`
+
+const Input = styled.input`
+    position: relative;
+    z-index: 1;
+    width: 326px;
+    height: 58px;
+    left: 25px;
+    top:90px;
+    margin-bottom:13px;
+    border-radius: 5px;
+    background-color: #FFFFFF;
+    
+    font-family: 'Saira Stencil One', cursive;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    border: 1px solid #5C3782;
+    cursor: text;
+    
+    
+
+    &::placeholder {
+        position: relative;
+        color: #000000;
+        width: 60px;
+        height: 23px;
+        left: 15px;
+        font-family: 'Saira Stencil One', cursive;
+        font-style: normal;
+        font-weight: 400,
+        font-size: 20px;
+        line-height: 23px;
+    }
+`
+const Button = styled.button`
+    background-color: #5C3782;
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    border: 1px solid #5C3782;
+    width: 300px;
+    height: 46px;
+    left:39px;
+    top: 90px;
+    cursor: pointer;
+
+    p{
+        position: relative;
+        z-index: 2;
+        font-family: 'Saira Stencil One', cursive;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 23px;
+        color: #FFFFFF;
+    }
 `
