c0853e2edb57ded58e192de58c81ad34f4c35675
Thyago Lucas <Salc@icomp.ufam.edu.br>
2022-05-11 00:40:00 -0400

    commit de troca parcial de pc

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,27 +9,24 @@ function Register (){
 
     const navigate = useNavigate();
 
-    const [datasToRegister, setDatas] = useState({name:'', email:'', password:'', confPassword:''});
-
+    const [datasToRegister, setDatas] = useState({name:'', email:'', password:'', passwordConf:''});
+    console.l
     function tryRegister(event){
 
         event.preventDefault();
 
         api
         .post('/register', datasToRegister)
-        .then((response)=>{
-            console.log('O registro ta safe!', response.data);
-        })
-        .catch((err) => {
-            console.log('Erro ao cadastrar usuario', err)
-        })
+            .then((response)=>{
+                console.log('O registro ta safe!', response.data);
+            })
+            .catch((err) => {
+                console.log('Erro ao cadastrar usuario', err)
+            })
 
     }
 
-    function toLogin(){
-        navigate('/');
-    }
-    
+
 
     return(
         <>
@@ -37,10 +34,10 @@ function Register (){
 
             <Main>
                 <form onSubmit={tryRegister}>
-                    <input type={'text'} placeholder={'Nome'}></input>
-                    <input type={'email'} placeholder={'E-mail'}></input>
-                    <input type={'password'} placeholder={'Senha'}></input>
-                    <input type={'password'} placeholder={'Confirme sua senha'}></input>
+                    <input type={'text'} placeholder={'Nome'} onChange={(e)=>{setDatas({...datasToRegister, name:e.target.value})}}></input>
+                    <input type={'email'} placeholder={'E-mail'} onChange={(e)=>{setDatas({...datasToRegister, email:e.target.value})}}></input>
+                    <input type={'password'} placeholder={'Senha'} onChange={(e)=>{setDatas({...datasToRegister, password:e.target.value})}}></input>
+                    <input type={'password'} placeholder={'Confirme sua senha'} onChange={(e)=>{setDatas({...datasToRegister, passwordConf:e.target.value})}}></input>
 
                     <button>cadastrar</button>
                 </form>
