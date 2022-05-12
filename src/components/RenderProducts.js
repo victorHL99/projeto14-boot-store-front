872eaa54d2a2dd0aa469d96f9786087d93e6dba7
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-12 17:09:27 -0300

    Arrumando layout da pagina de produtos

--- src/components/RenderProducts.js
+++ src/components/RenderProducts.js
@@ -1,12 +1,30 @@
-import React from 'react';
-
+import styled from 'styled-components';
 export default function RenderProducts({img,name, price}) {
     return (
-        <div>
-            <img src={img} alt=""/>
+        <ContainerProducts>
+            <ImagesProducts src={img} alt=""/>
             <h1>{name}</h1>
             <h1>{price}</h1>
             
-        </div>
+        </ContainerProducts>
     )
 }
+
+const ContainerProducts = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+`
+
+const ImagesProducts = styled.img`
+    position: relative;
+    left: 0;
+    width: 200px;
+    height: 200px;
+    border-radius: 20%;
+    background-color: none;
+
+
+`
